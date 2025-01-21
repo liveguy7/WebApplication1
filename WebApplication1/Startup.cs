2eb6d77733a6930f9ce3552dfa50258127f864c0
jerom <jerom@LAPTOP-L0BU5SNG>
2025-01-20 20:37:42 -0500

    Add project files.

---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response
+                    .WriteAsync("Jello");
+            });
+        }
+    }
+
+}
+
+
