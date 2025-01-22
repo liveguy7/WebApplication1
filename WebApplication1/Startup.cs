3b55de1ecc4744c867555f86bd6c5e53cbdc35f4
jerom <jerom@LAPTOP-L0BU5SNG>
2025-01-21 19:14:40 -0500

    c4

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,25 +1,49 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace WebApplication1
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+
+        }
         public void ConfigureServices(IServiceCollection services)
         {
 
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+                              ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW1: incoming..");
+                await next();
+                logger.LogInformation("MW1: outgoing..");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW2: incoming..");
+                await next();
+                logger.LogInformation("MW2: outgoing..");
+            });
+
             app.Run(async (context) =>
             {
                 await context.Response
-                    .WriteAsync("Jello");
+                    .WriteAsync("Jello!");
+                logger.LogInformation("MW3!");
             });
         }
     }
